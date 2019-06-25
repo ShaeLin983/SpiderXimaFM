ca60b2f10443b383ab3c3daec4f3ba8312d5c8c0
shae <linx0220@163.com>
2019-06-25 23:39:42 +0800

    解决audio放在循环外还是循环内的问题

--- SpiderXimaFM.py
+++ SpiderXimaFM.py
@@ -25,10 +25,17 @@ class SpiderXimaFM(object):
     def parse_data(self, dict_data):
 
         for audio_data in dict_data:
+
+            # audio不能放在for循环外面，因为append添加的是对象的引用，不是对象的值
+            # audio放在外面的话，一直是一个对象一个地址，audio值刷新的话，前面append的值都会刷新
+            # 放在for循环里面，才能创建新的audio对象，前面创建的audio才不会被刷新
+
             audio = {}
             audio["name"] = audio_data['trackName']
             audio["src"] = audio_data['src']
             self.audio_list.append(audio)
+
+            print(self.audio_list)
 
     def save_data(self):
         for audio in self.audio_list:
@@ -36,7 +43,7 @@ class SpiderXimaFM(object):
             file_url = audio['src']
             print(file_name)
             with open("audio/{}.m4a".format(file_name), "wb")as f:
-                f.write(requests.get(file_url, headers=self.headers).content)
+                 f.write(requests.get(file_url, headers=self.headers).content)
 
     # 指定爬取的页码（起始页-结束页）
     def run(self, page_start=1, page_end=2):
